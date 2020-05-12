23f5a68f5d818a8bce3874cee15f21bf49c1b9ce
ofersarid <ofersarid@gmail.com>
2020-05-12 17:25:45 +0300

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -55,7 +55,7 @@ class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps, store, isServer, homeData } = this.props;
+    const { Component, pageProps, store, isServer } = this.props;
     return (
       <Provider store={store} >
         <Helmet title="Delta | Hire an elite front team on-demand" description="When you need to get to your next business milestone quickly, hiring Senior Developers, PMs & Designers is not an simple operation. Delta is an experienced team of advanced technologists who love working together - and we are ready to drive your projects forward." imageForSocial="/images/delta-logo-social-square.png" />
