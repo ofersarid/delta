73495cf5da8c842ad1c609607fa4816dd9ffa558
ofersarid <ofersarid@gmail.com>
2020-05-24 14:39:32 +0300

    typo fix

--- services/home.js
+++ services/home.js
@@ -2,9 +2,9 @@ import { fromJS } from 'immutable';
 
 const reducer = (state = fromJS({
   tabTitle: 'Delta | Hire an elite front team on-demand',
-  metaDescription: 'When you need to get to your next business milestone quickly, hiring Senior Developers, PMs & Designers is not an simple operation. Delta is an experienced team of advanced technologists who love working together - and we are ready to drive your projects forward.',
+  metaDescription: 'When you need to get to your next business milestone quickly, hiring Senior Developers, PMs & Designers is not an simple operation. Delta is an experienced team of advanced technologists who love working together - and are ready to drive your projects forward.',
   heroTitle: 'Hire an elite front team on-demand',
-  heroParagraph: 'When you need to get to your next business milestone quickly, hiring Senior Developers, PMs & Designers is not an simple operation.\nDelta is an experienced team of advanced technologists who love working together - and we are ready to drive your projects forward.'
+  heroParagraph: 'When you need to get to your next business milestone quickly, hiring Senior Developers, PMs & Designers is not an simple operation.\nDelta is an experienced team of advanced technologists who love working together - and are ready to drive your projects forward.'
 }), action) => {
   switch (action.type) {
     case 'HOME:UPDATE':
