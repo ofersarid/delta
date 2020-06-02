ea4ad30a39cc95212c6ca37d2c2f11175b2d4fea
ofersarid <ofersarid@gmail.com>
2020-06-02 21:07:54 +0300

    wip

--- containers/estimator/estimator.js
+++ containers/estimator/estimator.js
@@ -7,19 +7,33 @@ import styles from './styles.scss';
 import { device } from '../../services';
 
 const Estimator = ({ device }) => {
+  const [selectedStep, setSelectedStep] = useState(1);
+
   return (
     <div className={cx(styles.estimator)} >
       <h1 >Generate Estimate</h1 >
-      {device === 'mobile' ? null : (
-        <div className={styles.calculator}>
-          <Step index={1} options={['Lorem1', 'Ipsum1', 'Dollar1']} focus={true} />
-          <Step index={2} options={['Lorem2', 'Ipsum2', 'Dollar2']} />
-          <Step index={3} options={['Lorem3', 'Ipsum3', 'Dollar3']} />
-          <Step index={4} options={['Lorem4', 'Ipsum4', 'Dollar4']} />
-          <Step index={5} options={['Lorem5', 'Ipsum5', 'Dollar5']} />
-        </div>
-      )}
-    </div>
+      <div className={styles.calculator}>
+        <Step index={1} options={['Lorem1', 'Ipsum1', 'Dollar1']} focus={selectedStep === 1} />
+        <Step index={3} options={['Lorem3', 'Ipsum3', 'Dollar3']} focus={selectedStep === 2} />
+        <Step index={2} options={['Lorem2', 'Ipsum2', 'Dollar2']} focus={selectedStep === 3} />
+        <Step index={4} options={['Lorem4', 'Ipsum4', 'Dollar4']} focus={selectedStep === 4} />
+        <Step index={5} options={['Lorem5', 'Ipsum5', 'Dollar5']} focus={selectedStep === 5} />
+      </div>
+      <div className={styles.controllers}>
+        <button
+          className={cx(styles.prev, { [styles.disable]: selectedStep === 1 })}
+          onClick={() => {
+            setSelectedStep(Math.max(selectedStep - 1, 1));
+          }}
+        >Prev</button >
+        <button
+          className={cx(styles.next, { [styles.disable]: selectedStep === 5 })}
+          onClick={() => {
+            setSelectedStep(Math.min(selectedStep + 1, 5));
+          }}
+        >Next</button >
+      </div >
+    </div >
   );
 };
 
