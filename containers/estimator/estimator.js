f580a48b76db6d1d00b1766c8bbb62856e0aff9c
ofersarid <ofersarid@gmail.com>
2020-06-02 20:14:18 +0300

    wip

--- containers/estimator/estimator.js
+++ containers/estimator/estimator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'; // eslint-disable-line
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import cx from 'classnames';
@@ -12,7 +12,11 @@ const Estimator = ({ device }) => {
       <h1 >Generate Estimate</h1 >
       {device === 'mobile' ? null : (
         <div className={styles.calculator}>
-          <Step options={['Lorem', 'Ipsum', 'Dollar']} />
+          <Step index={1} options={['Lorem1', 'Ipsum1', 'Dollar1']} focus={true} />
+          <Step index={2} options={['Lorem2', 'Ipsum2', 'Dollar2']} />
+          <Step index={3} options={['Lorem3', 'Ipsum3', 'Dollar3']} />
+          <Step index={4} options={['Lorem4', 'Ipsum4', 'Dollar4']} />
+          <Step index={5} options={['Lorem5', 'Ipsum5', 'Dollar5']} />
         </div>
       )}
     </div>
--- containers/estimator/step.js
+++ containers/estimator/step.js
@@ -1,38 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'; // eslint-disable-line
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import cx from 'classnames';
 import styles from './styles.scss';
 import { device } from '../../services';
 
-const Step = ({ device, options }) => {
+const Step = ({ device, options, focus, index }) => {
   const [selectedOption, setSelectedOption] = useState(0);
 
-
   return device === 'mobile' ? null : (
-    <div className={styles.step}>
+    <div className={cx(styles.step, { [styles.focused]: focus })}>
       <div className={styles.top}>
         <div className={styles.slider}>
           <div className={styles.ball} style={{
             top: `${selectedOption * 49}px`,
           }} />
         </div>
-        <ul className={styles.options}>
-          {options.map((itm, i) => (
-            <li
-              key={itm}
-              className={cx({ [styles.selected]: i === selectedOption })}
-              onClick={() => {
-                setSelectedOption(i);
-              }}
-            >
-              {itm}
-            </li>
-          ))}
-        </ul>
+        {focus && (
+          <ul className={styles.options}>
+            {options.map((itm, i) => (
+              <li
+                key={itm}
+                className={cx({ [styles.selected]: i === selectedOption })}
+                onClick={() => {
+                  setSelectedOption(i);
+                }}
+              >
+                {itm}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className={styles.bottom}>
-        <div className={styles.index}>1</div>
+        <div className={styles.index}>{index}</div>
         <div className={styles.line}></div>
       </div>
     </div>
