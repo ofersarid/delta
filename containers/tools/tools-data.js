a18d9316f730fa7c567a4c66d1fed0ebbb2f98d3
ofersarid <ofersarid@gmail.com>
2020-09-03 12:50:18 +0300

    added flutter and vue to tools section

--- containers/tools/tools-data.js
+++ containers/tools/tools-data.js
@@ -1,67 +1,104 @@
 export default {
-  tech: [{
-    title: 'React',
-    thumb: '/images/tools/react.png',
-  }, {
-    title: 'Redux',
-    thumb: '/images/tools/redux.png',
-  }, {
-    title: 'Next.js',
-    thumb: '/images/tools/next.png',
-  }, {
-    title: 'Firebase',
-    thumb: '/images/tools/firebase.png',
-  }, {
-    title: 'Saas.js',
-    thumb: '/images/tools/sass.png',
-  }, {
-    title: 'CSS',
-    thumb: '/images/tools/css-modules.png',
-  }, {
-    title: 'Webpack',
-    thumb: '/images/tools/webpack.png',
-  }, {
-    title: 'Parcel',
-    thumb: '/images/tools/parcel.png',
-  }],
-  uxUi: [{
-    title: 'Adobe XD',
-    thumb: '/images/tools/xd.png',
-  }, {
-    title: 'Figma',
-    thumb: '/images/tools/figma.png',
-  }, {
-    title: 'Zeplin',
-    thumb: '/images/tools/zeplin.png',
-  }],
-  visual: [{
-    title: 'Illustrator',
-    thumb: '/images/tools/illustrator.png',
-  }, {
-    title: 'Photoshop',
-    thumb: '/images/tools/photoshop.png',
-  }, {
-    title: '3DS Max',
-    thumb: '/images/tools/3ds.png',
-  }, {
-    title: 'Keyshot',
-    thumb: '/images/tools/keyshot.png',
-  }],
-  code: [{
-    title: 'Github',
-    thumb: '/images/tools/github.png',
-  }, {
-    title: 'Bit Bucket',
-    thumb: '/images/tools/bit-bucket.png',
-  }],
-  workflow: [{
-    title: 'Trello',
-    thumb: '/images/tools/trello.png',
-  }, {
-    title: 'Jira',
-    thumb: '/images/tools/jira.png',
-  }, {
-    title: 'Slack',
-    thumb: '/images/tools/slack.png',
-  }],
+  tech: [
+    {
+      title: 'Flutter',
+      thumb: '/images/tools/flutter.png'
+    },
+    {
+      title: 'Vue',
+      thumb: '/images/tools/vue.png'
+    },
+    {
+      title: 'Vuex',
+      thumb: '/images/tools/vuex.png'
+    },
+    {
+      title: 'React',
+      thumb: '/images/tools/react.png'
+    },
+    {
+      title: 'Redux',
+      thumb: '/images/tools/redux.png'
+    },
+    {
+      title: 'Next.js',
+      thumb: '/images/tools/next.png'
+    },
+    {
+      title: 'Firebase',
+      thumb: '/images/tools/firebase.png'
+    },
+    {
+      title: 'Saas.js',
+      thumb: '/images/tools/sass.png'
+    },
+    {
+      title: 'CSS',
+      thumb: '/images/tools/css-modules.png'
+    },
+    {
+      title: 'Webpack',
+      thumb: '/images/tools/webpack.png'
+    },
+    {
+      title: 'Parcel',
+      thumb: '/images/tools/parcel.png'
+    }
+  ],
+  uxUi: [
+    {
+      title: 'Adobe XD',
+      thumb: '/images/tools/xd.png'
+    },
+    {
+      title: 'Figma',
+      thumb: '/images/tools/figma.png'
+    },
+    {
+      title: 'Zeplin',
+      thumb: '/images/tools/zeplin.png'
+    }
+  ],
+  visual: [
+    {
+      title: 'Illustrator',
+      thumb: '/images/tools/illustrator.png'
+    },
+    {
+      title: 'Photoshop',
+      thumb: '/images/tools/photoshop.png'
+    },
+    {
+      title: '3DS Max',
+      thumb: '/images/tools/3ds.png'
+    },
+    {
+      title: 'Keyshot',
+      thumb: '/images/tools/keyshot.png'
+    }
+  ],
+  code: [
+    {
+      title: 'Github',
+      thumb: '/images/tools/github.png'
+    },
+    {
+      title: 'Bit Bucket',
+      thumb: '/images/tools/bit-bucket.png'
+    }
+  ],
+  workflow: [
+    {
+      title: 'Trello',
+      thumb: '/images/tools/trello.png'
+    },
+    {
+      title: 'Jira',
+      thumb: '/images/tools/jira.png'
+    },
+    {
+      title: 'Slack',
+      thumb: '/images/tools/slack.png'
+    }
+  ]
 };
